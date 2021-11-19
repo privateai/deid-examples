cb19841f131f98c96455079ab191377aab7a6d84
Onur Cayci <onur@private-ai.com>
2021-11-19 09:18:25 -0500

    PAI-27 implement example script Fake entity generation

---
+++ python/examples/fake_entity_generation.py
@@ -0,0 +1,26 @@
+import os
+import pprint
+
+import requests
+import dotenv
+
+# Use to load the API KEY for authentication
+dotenv.load_dotenv()
+
+# Make the POST request to the docker container
+response = requests.post(
+    url="http://localhost:8080/deidentify_text",
+    json={
+        "text": "My name is John and my friend is Grace and we live in Barcelona",
+        "key": os.getenv("API_KEY", ""),
+        "fake_entity_accuracy_mode": "standard"
+    }
+)
+
+# Check if the HTTP request was successful
+if response.ok:
+    pprint.pprint(response.json())
+else:
+    raise Exception(
+        f"The request failed with the status code {response.status_code}"
+    )
