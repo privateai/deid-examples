81fb1d20f2f6e08e61b344cd74a39cf7091b93bf
Onur Cayci <onur@private-ai.com>
2021-12-06 15:08:09 -0500

    PAI-27 add comments to the existing JS scripts to include usage and setup guides

--- js/examples/fake_entity_generation.js
+++ js/examples/fake_entity_generation.js
@@ -1,3 +1,14 @@
+/**
+ * Example script to illustrate how to make API calls to the Private AI Docker
+ * container to deidentify text using the fake entity generation feature.
+ *
+ * To use this script, please start the Docker container locally, as per the
+ * instructions at https://private-ai.com/docs/installation.
+ *
+ * In order to use the API key issued by Private AI, you can run the script as
+ * `API_KEY=<your key here> node fake_entity_generation.js` or you can define a
+ * `.env` file which has the line `API_KEY=<your key here>`.
+ */
 const axios = require("axios");
 const dotenv = require("dotenv");
 
