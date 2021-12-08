c278673bcaa4140d3c7060bd2594863f2859c957
Onur Cayci <onur@private-ai.com>
2021-12-08 09:36:44 -0500

    PAI-27 simplify asyncio script example

---
+++ python/examples/async_call.py
@@ -0,0 +1,73 @@
+# Example script to illustrate how to make API calls to the Private AI Docker
+# container to deidentify text.
+#
+# To use this script, please start the Docker container locally, as per the
+# instructions at https://private-ai.com/docs/installation.
+#
+# In order to use the API key issued by Private AI< you can run the script as
+# `API_KEY=<your key here> python async_call.py` or you can define a `.env` file
+# which has the line `API_KEY=<your key here>`.
+import os
+import pprint
+import asyncio
+from typing import Dict
+
+import aiohttp
+import requests
+import dotenv
+
+# Use to load the API key for authentication
+dotenv.load_dotenv()
+
+# Define an asynchronous function using the aiohttp library.
+
+
+async def async_aiohttp_call() -> None:
+
+    # create an asynchronous aiohttp client session
+    async with aiohttp.ClientSession() as session:
+
+        # create an asynchronous aiohttp post call
+        async with session.post(
+            url="http://localhost:8080/deidentify_text",
+            json={
+                "text": "My name is John and my friend is Grace.",
+                "key": os.getenv("API_KEY", "")
+            }
+        ) as response:
+
+            # print the deidentified text
+            pprint.pprint(await response.json())
+
+
+async def async_post(
+    url: str,
+    json: Dict[str, str]
+) -> requests.Response:
+    return requests.post(
+        url=url,
+        json=json
+    )
+
+
+async def async_requests_call() -> None:
+    response = await async_post(
+        url="http://localhost:8080/deidentify_text",
+        json={
+            "text": "My name is John and my friend is Grace.",
+            "key": os.getenv("API_KEY", "")
+        }
+    )
+
+    # check for errors
+    if response.ok:
+        pprint.pprint(response.json())
+    else:
+        raise Exception(
+            f"The request failed with the status code {response.status_code}"
+        )
+
+
+if __name__ == "__main__":
+    asyncio.run(async_aiohttp_call())
+    asyncio.run(async_requests_call())
