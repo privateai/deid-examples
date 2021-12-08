42315c457d144931ef41d3d5edc16a95c3ea696b
Onur Cayci <onur@private-ai.com>
2021-12-08 11:16:22 -0500

    PAI-27 fix comments

--- python/examples/concurrency.py
+++ python/examples/concurrency.py
@@ -1,11 +1,11 @@
 # Example script to illustrate how to make API calls to the Private AI Docker
-# container to deidentify a text using the enabled classes feature.
+# container to deidentify a text using concurrency.
 #
 # To use this script, please start the Docker container locally, as per the
 # instructions at https://private-ai.com/docs/installation.
 #
 # In order to use the API key issued by Private AI, you can run the script as
-# `API_KEY=<your key here> python enabled_classes.py` or you can define a `.env`
+# `API_KEY=<your key here> python concurrency.py` or you can define a `.env`
 # file which has the line`API_KEY=<your key here>`.
 import os
 import pprint
