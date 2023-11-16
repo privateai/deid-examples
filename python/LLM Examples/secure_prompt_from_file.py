2a34480f40ef79dc86378929e004625c9b8760d4
a-guiducci <adam.guiducci@private-ai.com>
2023-11-16 13:37:11 -0500

    updated openai logic to use their new client

--- python/LLM Examples/secure_prompt_from_file.py
+++ python/LLM Examples/secure_prompt_from_file.py
@@ -1,12 +1,12 @@
-from privateai_client import PAIClient
-from privateai_client import request_objects
-import openai
-import sys
-import demo_config
 import argparse
-import json
 import base64
+import json
 import os
+import sys
+
+import demo_config
+from openai import OpenAIClient
+from privateai_client import PAIClient, request_objects
 
 # Initialize parser
 parser = argparse.ArgumentParser(description="Secure LLM prompting from a file")
@@ -20,26 +20,23 @@ PRIVATEAI_API_KEY = demo_config.privateai["PROD_KEY"]
 PRIVATEAI_URL = demo_config.privateai["PROD_URL"]
 
 # Initialize the openai client
-openai.api_key = demo_config.openai["API_KEY"]
+openai_client = OpenAIClient(api_key=demo_config.openai["API_KEY"])
 
-file_dir = args.directory 
-file_name = args.filename 
-filepath = args.file 
-file_type= args.filetype.split("/")[1] 
-PRIVATEAI_SCHEME = 'https'
+file_dir = args.directory
+file_name = args.filename
+filepath = args.file
+file_type = args.filetype.split("/")[1]
+PRIVATEAI_SCHEME = "https"
 client = PAIClient(PRIVATEAI_SCHEME, PRIVATEAI_URL)
 client.add_api_key(PRIVATEAI_API_KEY)
 
 
 def prompt_chat_gpt(text):
-    completion = openai.ChatCompletion.create(
-        model="gpt-4",
-        messages=[
-        {"role": "user",
-         "content": text}
-        ]
+    completion = openai_client.chat.completions.create(
+        model="gpt-4", messages=[{"role": "user", "content": text}]
     )
-    return completion.choices[0].message['content']
+    return completion.choices[0].message.content
+
 
 # Read from file
 with open(filepath, "rb") as b64_file:
@@ -53,7 +50,7 @@ redaction_response = client.process_files_base64(request_object=request_obj)
 entity_list = redaction_response.get_reidentify_entities()
 
 # Write to file
-with open(os.path.join(file_dir,f"{file_name}.{file_type}"), 'wb') as redacted_file:
+with open(os.path.join(file_dir, f"{file_name}.{file_type}"), "wb") as redacted_file:
     processed_file = redaction_response.processed_file.encode("ascii")
     processed_file = base64.b64decode(processed_file, validate=True)
     redacted_file.write(processed_file)
@@ -61,11 +58,15 @@ with open(os.path.join(file_dir,f"{file_name}.{file_type}"), 'wb') as redacted_f
 print("\n**************************************************\n")
 print(f"redacted file contents: {redaction_response.processed_text}\n")
 
-file_summary = prompt_chat_gpt(f"summarize this file: {redaction_response.processed_text}")
+file_summary = prompt_chat_gpt(
+    f"summarize this file: {redaction_response.processed_text}"
+)
 print("\n SUMMARY \n")
 print("********************** redacted summary **********************")
 print(file_summary)
 print("********************** REID summary **********************")
-reid_req_obj = request_objects.reidentify_text_obj(processed_text=[file_summary],entities=entity_list)
+reid_req_obj = request_objects.reidentify_text_obj(
+    processed_text=[file_summary], entities=entity_list
+)
 reidentification_response_obj = client.reidentify_text(reid_req_obj)
 print(reidentification_response_obj.body)
