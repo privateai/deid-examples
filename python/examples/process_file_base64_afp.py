3590e5a1c1f32ec4580eb9c39ce98904c293803e
Bryan Bell-Smith <bryan@private-ai.com>
2025-05-06 16:53:06 -0400

    Refactor this script to be standalone, synchronous, and a bit simpler

--- python/examples/process_file_base64_afp.py
+++ python/examples/process_file_base64_afp.py
@@ -1,27 +1,146 @@
-# Example script to illustrate how to make API calls to the Private AI API
+# Example script to illustrate how to make async API calls to the Private AI API
 # to deidentify a provided file via base64 encoding.
 
+import base64
+import os
 import time
-from helpers.process_file_helpers import submit_job, get_status, download_file
+
+import requests
+from dotenv import load_dotenv
+
+SUPPORTED_FILE_TYPES = {
+    ".pdf": "application/pdf",
+    ".json": "application/json",
+    ".xml": "application/xml",
+    ".csv": "text/csv",
+    ".doc": "application/msword",
+    ".docx": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    ".eml": "message/rfc822",
+    ".txt": "text/plain",
+    ".xls": "application/vnd.ms-excel",
+    ".xlsx": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    ".ppt": "application/vnd.ms-powerpoint",
+    ".pptx": "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+    ".dcm": "application/dicom",
+    ".jpg": "image/jpeg",
+    ".jpeg": "image/jpeg",
+    ".tif": "image/tiff",
+    ".tiff": "image/tiff",
+    ".png": "image/png",
+    ".bmp": "image/bmp",
+    ".wav": "audio/wav",
+    ".mp3": "audio/mpeg",
+    ".mp4": "audio/mp4",
+    ".m4a": "audio/m4a",
+    ".webm": "audio/webm",
+}
+
+load_dotenv()
+
+# Check if the API_KEY and PAI_URL environment variables are set
+if "PAI_URL" not in os.environ:
+    raise KeyError("PAI_URL must be defined in your environment to run the examples.")
+if "USER_ID" not in os.environ:
+    raise KeyError("USER_ID must be defined in your environment to run the examples.")
+
+PRIVATE_AI_URL = os.getenv("PAI_URL")
+USER_ID = os.getenv("USER_ID")
 
 FILE_NAME = "PAI_SYNTH_EN_medical-referral_2.pdf"
 
-# Start timer
-stt = time.time()
+INPUT_DIR_PATH = "./data"
+OUTPUT_DIR_PATH = "./output"
 
-# Submit file for processing and get job id
-job_id = submit_job(FILE_NAME)
 
-# Poll for job completion
-while True:
-    if get_status(job_id, FILE_NAME):
-        break
-    time.sleep(5)
+def submit_job(file_request: dict) -> str:
+    response = requests.post(f"{PRIVATE_AI_URL}/process/files/base64", json=file_request, headers={"user-id": USER_ID})
 
-# Download deidentified file
-download_file(job_id, FILE_NAME)
+    response.raise_for_status()
 
-# Calculate and display total elapsed time
-elapsed_time = time.time() - stt
-minutes, seconds = divmod(int(elapsed_time), 60)
-print(f"Total elapsed time: {minutes} minutes and {seconds} seconds")
+    # Get the job ID
+    job_id = response.json().get("job_id")
+
+    if not job_id:
+        raise ValueError(f"No job ID received in response: {response.json()}")
+
+    return job_id
+
+
+def get_state(job_id: str) -> tuple[str, str]:
+    session = requests.Session()
+    with session.get(f"{PRIVATE_AI_URL}/jobs/{job_id}/state", headers={"user-id": USER_ID}) as response:
+        response.raise_for_status()
+        response_data = response.json()
+        return response_data.get("state"), response_data.get("description")
+
+
+def download_file(job_id: str, output_file_name: str) -> None:
+    download_url = f"{PRIVATE_AI_URL}/jobs/{job_id}"
+    with requests.get(download_url, headers={"user-id": USER_ID}) as response:
+        response.raise_for_status()
+
+        processed_file = response.json().get("processed_file")
+
+        decoded_file = base64.b64decode(processed_file, validate=True)
+
+        output_path = os.path.join("./output", f"Redacted {output_file_name}")
+        with open(output_path, "wb") as redacted_file:
+            redacted_file.write(decoded_file)
+
+        print(f"Successfully downloaded redacted file: Redacted {output_file_name}")
+
+
+def process_file(file_name: str) -> None:
+    print(f"Processing {file_name}")
+
+    # Opening file to build request json
+    _, ext = os.path.splitext(file_name)
+
+    if ext not in SUPPORTED_FILE_TYPES:
+        raise ValueError(f"File {file_name} not supported and will not be deidentified.")
+    else:
+        filepath = os.path.join(INPUT_DIR_PATH, file_name)
+        file_type = SUPPORTED_FILE_TYPES[ext]
+
+        # Read from file
+        with open(filepath, "rb") as b64_file:
+            file_data = base64.b64encode(b64_file.read()).decode()
+
+        request_data = {
+            "file": {
+                "data": file_data,
+                "content_type": file_type,
+            }
+        }
+
+    # Submit file for processing and get job id
+    job_id = submit_job(request_data)
+
+    # Poll for job completion
+    while True:
+        state, description = get_state(job_id)
+        if state in ["completed", "downloaded"]:
+            print(f"Deidentification of {file_name} finished")
+            break
+        elif state in ["failed", "expired", "cancelled"]:
+            print(f"Deidentification of {file_name} failed with state: {state}")
+            raise ValueError(f"Error description: {description}")
+        else:
+            print(f"Deidentification of {file_name} state: {state}")
+        time.sleep(5)
+
+    # Download deidentified file
+    download_file(job_id, output_file_name=f"redacted-{file_name}")
+
+
+if __name__ == "__main__":
+
+    # Start timer
+    stt = time.time()
+
+    process_file(FILE_NAME)
+
+    # Calculate and display total elapsed time
+    elapsed_time = time.time() - stt
+    minutes, seconds = divmod(int(elapsed_time), 60)
+    print(f"Total elapsed time: {minutes} minutes and {seconds} seconds")
